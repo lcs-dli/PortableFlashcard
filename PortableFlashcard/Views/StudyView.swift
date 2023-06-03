16f34fc65f5b6cc5b5c91a4d6305700b3a4d936d
David Li <dtli@lcs.on.ca>
2023-06-03 18:39:09 -0400

    Create studyView for the next step of deckView. Finish its draft

---
+++ PortableFlashcard/Views/StudyView.swift
@@ -0,0 +1,56 @@
+//
+//  StudyView.swift
+//  PortableFlashcard
+//
+//  Created by David Li on 2023-06-03.
+//
+
+import SwiftUI
+
+struct StudyView: View {
+    //MARK: Storing property
+    let new: Int
+    let learn: Int
+    let review: Int
+    //MARK: Computing property
+    var body: some View {
+        //if there is no cards for study
+        if((new + learn + review) == 0){
+            
+            
+            Text("Congratulation, you have nothing to study for now! ")
+            
+        
+        //if there is cards for study
+        }else{
+            VStack{
+                HStack{
+                    Text("Deckname")
+                    VStack{
+                        Text("New: \(new)")
+                        Text("Learn: \(learn)")
+                        Text("Review: \(review)")
+                    }
+                }
+                
+                Spacer()
+                
+                //button for begin of study
+                Button(action: {
+                    
+                }, label: {
+                    HStack{
+                        Text("Study Now")
+                        Image(systemName: "arrow.forward")
+                    }
+                })
+            }
+        }
+    }
+}
+
+struct StudyView_Previews: PreviewProvider {
+    static var previews: some View {
+        StudyView(new: 0, learn: 0, review: 1)
+    }
+}
