79ab241586e1abc84694d3e49fd363b9e72611b1
David Li <dtli@lcs.on.ca>
2023-06-03 01:58:12 -0400

    Initial Commit

---
+++ PortableFlashcard/PortableFlashcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortableFlashcardApp.swift
+//  PortableFlashcard
+//
+//  Created by David Li on 2023-06-03.
+//
+
+import SwiftUI
+
+@main
+struct PortableFlashcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
