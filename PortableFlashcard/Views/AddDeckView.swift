bd2a6057a72dd1ae3ec2958e35bc876c5b305ca0
David Li <dtli@lcs.on.ca>
2023-06-09 13:14:04 -0400

    Create toolbar and addDeck function to manage the code and connect with the database

--- PortableFlashcard/Views/AddDeckView.swift
+++ PortableFlashcard/Views/AddDeckView.swift
@@ -6,17 +6,71 @@
 //
 
 import SwiftUI
+import Blackbird
 
 struct AddDeckView: View {
     //MARK: Storing property
+    //Access to the connection of the database (needed to add a new record)
+    @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
+    
     @State var deckName = ""
     //MARK: Computing property
     var body: some View {
-        VStack{
-            Text("Deck's name: ")
-            
-            TextField("", text: $deckName)
-                .textFieldStyle(.roundedBorder)
+        NavigationView{
+            VStack{
+                Text("Deck's name: ")
+                
+                TextField("", text: $deckName)
+                    .textFieldStyle(.roundedBorder)
+            }
+            .padding(5)
+            .toolbar{
+                ToolbarItem(placement: .primaryAction){
+                    Button(action: {
+                        addDeck()
+                    }, label: {
+                        Text("Add")
+                    })
+                }
+            }
+        }
+        
+    }
+    
+    //MARK: Functions
+    
+    func addDeck(){
+        //Write to the database
+        Task{
+            try await db?.transaction{core in
+                try core.query("""
+                            INSERT INTO DECKS (
+                                Deck,
+                                New,
+                                Learn,
+                                Review,
+                                Remembered
+                                Due,
+                                Total
+                            )
+                            VALUES(
+                                (?),
+                                (?),
+                                (?),
+                                (?),
+                                (?),
+                                (?),
+                                (?)
+                            )
+                        """,
+                        deckName,
+                        0,
+                        0,
+                        0,
+                        0,
+                        0,
+                        0)
+            }
         }
     }
 }
@@ -24,5 +78,6 @@ struct AddDeckView: View {
 struct AddDeckView_Previews: PreviewProvider {
     static var previews: some View {
         AddDeckView()
+            .environment(\.blackbirdDatabase, AppDatabase.instance)
     }
 }
