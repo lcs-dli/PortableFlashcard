9704582a71bc2f230bf003ba4f31a38dd7d19e81
David Li <dtli@lcs.on.ca>
2023-06-10 23:08:34 -0400

    Replaced list drawing from database with a static list.

--- PortableFlashcard/Views/BrowseView.swift
+++ PortableFlashcard/Views/BrowseView.swift
@@ -21,38 +21,18 @@ struct BrowseView: View {
     //MARK: Computing property
     
     var body: some View {
-        List(decks.results){ displayDeck in
-            NavigationLink(destination: {
-                BrowseSetView(selectedDeck: displayDeck, flashcards: findFlashcards(deckName: displayDeck.Deck))
-            }, label: {
-                HStack{
-                    Text(displayDeck.Deck)
-                    Spacer()
-                    Text("\(displayDeck.Total)")
+        NavigationView{
+            List(){
+                Text("A")
+                Text("B")
+                Text("C")
                 }
-                
-            })
-        }
-    }
-    
-    //MARK: Function
-    func findFlashcards(deckName: String) -> [Flashcards] {
-        var hold: [Flashcards] = []
-        for index in flashcards.results{
-            if let id = index["id"]?.intValue,
-                let front = index["Front"]?.stringValue,
-                let back = index["back"]?.stringValue,
-                let deck = index["Deck"]?.stringValue,
-                let status = index["Status"]?.stringValue,
-                let reviewDate = index["ReviewDate"]?.stringValue{
-                if(deck == deckName){
-                    hold.append(Flashcards(id: id, Front: front, Back: back, Deck: deck, Status: status, ReviewDate: reviewDate))
-                }
+            .navigationTitle("Browse")
             }
         }
-        return hold
+        
     }
-}
+    
 
 struct BrowseView_Previews: PreviewProvider {
     static var previews: some View {
